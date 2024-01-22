dab81b6fd7e08ad6d12ac6bece0ea9f48f965876
Quoc Dai <quocdai904@gmail.com>
2024-01-22 20:52:36 +0700

    Migrate db version 1.0

---
+++ src/Blog.Core/Domain/NewFolder/Identity/AppRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Core.Domain.NewFolder.Identity
+{
+    [Table("AppRoles")]
+    public class AppRole : IdentityRole<Guid>
+    {
+        public required string DisplayName { get; set; }
+    }
+}
